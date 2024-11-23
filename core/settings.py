0fb0f26cc9f43fc90ef07b19a18bbf9d2fa11374
PC <pc@ecf25caa-f651-451b-96ab-f2e8d5bc8a93.attlocal.net>
2024-11-23 02:25:12 -0600

    Fix SSL redirect settings

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,8 @@ MEDIA_URL = '/media/'
 
 # Security Settings for Production
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = False  # Cloudflare yönlendirmeyi yapacak
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
