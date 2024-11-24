21e89da6bc37bfbffff07927509dc88d1befa7ba
PC <pc@f55f23fb-9fcd-408a-bd3c-4abc54df9df3.attlocal.net>
2024-11-24 00:30:34 -0600

    Fix static files configuration

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Static ve Media dosya ayarları
 if DEBUG:
