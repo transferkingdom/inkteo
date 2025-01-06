4ddc611a8f5885fda32c18e7e8fc577f6033fffd
transferkingdom <tkweb@transferkingdom.com>
2025-01-06 17:46:36 -0600

    settings: whitenoise middleware kaldırıldı

--- core/settings.py
+++ core/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Whitenoise middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
